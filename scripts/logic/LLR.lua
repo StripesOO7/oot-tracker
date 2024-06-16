c8a019bc5b90a3bfb243a5e05c5819dcaaae4b07
StripesOO7 <54711792+StripeesOO7@users.noreply.github.com>
2024-06-16 23:27:50 +0200

    more overworld logic

--- scripts/logic/LLR.lua
+++ scripts/logic/LLR.lua
@@ -1,3 +1,93 @@
+Child_Lon_Lon_Ranch = OOTLocation.new("Child_Lon_Lon_Ranch")
+Child_LLR_Talons_House = OOTLocation.new("Child_LLR_Talons_House")
+Child_LLR_Stables = OOTLocation.new("Child_LLR_Stables")
+Child_LLR_Tower = OOTLocation.new("Child_LLR_Tower")
+Child_LLR_Grotto = OOTLocation.new("Child_LLR_Grotto")
+
+Adult_Lon_Lon_Ranch = OOTLocation.new("Adult_Lon_Lon_Ranch")
+Adult_LLR_Talons_House = OOTLocation.new("Adult_LLR_Talons_House")
+Adult_LLR_Stables = OOTLocation.new("Adult_LLR_Stables")
+Adult_LLR_Tower = OOTLocation.new("Adult_LLR_Tower")
+Adult_LLR_Grotto = OOTLocation.new("Adult_LLR_Grotto")
+
+Child_Lon_Lon_Ranch:connect_one_way("Song from Malon", function()
+    return All(
+        Has("ZeldasLetter"),
+        Has("Ocarina")
+    )
+end)
+Child_Lon_Lon_Ranch:connect_one_way("LLR Front Pot 1")
+Child_Lon_Lon_Ranch:connect_one_way("LLR Front Pot 2")
+Child_Lon_Lon_Ranch:connect_one_way("LLR Front Pot 3")
+Child_Lon_Lon_Ranch:connect_one_way("LLR Front Pot 4")
+Child_Lon_Lon_Ranch:connect_one_way("LLR Rain Shed Pot 1")
+Child_Lon_Lon_Ranch:connect_one_way("LLR Rain Shed Pot 2")
+Child_Lon_Lon_Ranch:connect_one_way("LLR Rain Shed Pot 3")
+Child_Lon_Lon_Ranch:connect_one_way("LLR Child Crate")
+Child_Lon_Lon_Ranch:connect_one_way("LLR GS Tree", function() return can_break_crate end)
+Child_Lon_Lon_Ranch:connect_one_way("LLR GS Rain Shed", function() return CanBonk() end)
+Child_Lon_Lon_Ranch:connect_one_way("LLR GS House Window", function() return Has("Boomerang") end)
+Child_Lon_Lon_Ranch:connect_one_way("LLR GS Back Wall", function() return Has("Boomerang") end)
+
+Adult_Lon_Lon_Ranch:connect_one_way("LLR GS House Window", function() return Has("Boomerang") end)
+Adult_Lon_Lon_Ranch:connect_one_way("LLR GS Back Wall", function() return Has("Boomerang") end)
+
+
+Child_Lon_Lon_Ranch:connect_one_way_entrance("Child Hyrule Field", Child_Hyrule_Fields)
+Child_Lon_Lon_Ranch:connect_one_way_entrance("Child LLR Talons House", Child_LLR_Talons_House) --at day
+Child_Lon_Lon_Ranch:connect_one_way_entrance("Child LLR Stables", Child_LLR_Stables)
+Child_Lon_Lon_Ranch:connect_one_way_entrance("Child LLR Tower", Child_LLR_Tower)
+Child_Lon_Lon_Ranch:connect_one_way_entrance("Child LLR Grotto", Child_LLR_Grotto)
+
+Adult_Lon_Lon_Ranch:connect_one_way_entrance("Adult Hyrule Field", Adult_Hyrule_Fields)
+Adult_Lon_Lon_Ranch:connect_one_way_entrance("Adult LLR Talons House", Adult_LLR_Talons_House)
+Adult_Lon_Lon_Ranch:connect_one_way_entrance("Adult LLR Stables", Adult_LLR_Stables)
+Adult_Lon_Lon_Ranch:connect_one_way_entrance("Adult LLR Tower", Adult_LLR_Tower)
+
+Child_LLR_Talons_House:connect_one_way("Child LLR Talons Chickens", function() return Has("ZeldasLetter") end)--at_day
+Child_LLR_Talons_House:connect_one_way("Child LLR Talons House Pot 1")
+Child_LLR_Talons_House:connect_one_way("Child LLR Talons House Pot 2")
+Child_LLR_Talons_House:connect_one_way("Child LLR Talons House Pot 3")
+
+Adult_LLR_Talons_House:connect_one_way("Adult LLR Talons Chickens")
+Adult_LLR_Talons_House:connect_one_way("Adult LLR Talons House Pot 1")
+Adult_LLR_Talons_House:connect_one_way("Adult LLR Talons House Pot 2")
+Adult_LLR_Talons_House:connect_one_way("Adult LLR Talons House Pot 3")
+
+Child_LLR_Talons_House:connect_one_way_entrance("Child Lon Lon Ranch", Child_Lon_Lon_Ranch)
+Adult_LLR_Talons_House:connect_one_way_entrance("Adult Lon Lon Ranch", Adult_Lon_Lon_Ranch)
+
+Child_LLR_Stables:connect_one_way("Child LLR Stables Left Cow", function() return CanPlay("EponasSong") end)
+Child_LLR_Stables:connect_one_way("Child LLR Stables Right Cow", function() return CanPlay("EponasSong") end)
+
+Adult_LLR_Stables:connect_one_way("Adult LLR Stables Left Cow", function() return CanPlay("EponasSong") end)
+Adult_LLR_Stables:connect_one_way("Adult LLR Stables Right Cow", function() return CanPlay("EponasSong") end)
+
+Child_LLR_Stables:connect_one_way_entrance("Child Lon Lon Ranch", Child_Lon_Lon_Ranch)
+Adult_LLR_Stables:connect_one_way_entrance("Adult Lon Lon Ranch", Adult_Lon_Lon_Ranch)
+
+Child_LLR_Tower:connect_one_way("Child LLR Freestanding PoH")
+Child_LLR_Tower:connect_one_way("Child LLR Tower Left Cow", function() return CanPlay("EponasSong") end)
+Child_LLR_Tower:connect_one_way("Child LLR Tower Right Cow", function() return CanPlay("EponasSong") end)
+
+Adult_LLR_Tower:connect_one_way("Aduld LLR Tower Left Cow", function() return CanPlay("EponasSong") end)
+Adult_LLR_Tower:connect_one_way("Aduld LLR Tower Right Cow", function() return CanPlay("EponasSong") end)
+
+Child_LLR_Tower:connect_one_way_entrance("Child Lon Lon Ranch", Child_Lon_Lon_Ranch)
+Adult_LLR_Tower:connect_one_way_entrance("Adult Lon Lon Ranch", Adult_Lon_Lon_Ranch)
+
+Child_LLR_Grotto:connect_one_way("Child LLR Deku Scrub Grotto Left", function() return can_stun_deku end)
+Child_LLR_Grotto:connect_one_way("Child LLR Deku Scrub Grotto Right", function() return can_stun_deku end)
+Child_LLR_Grotto:connect_one_way("Child LLR Deku Scrub Grotto Center", function() return can_stun_deku end)
+Child_LLR_Grotto:connect_one_way("Child LLR Grotto Beehive", function() return can_break_upper_beehive end )
+
+Adult_LLR_Grotto:connect_one_way("Adult LLR Deku Scrub Grotto Left", function() return can_stun_deku end)
+Adult_LLR_Grotto:connect_one_way("Adult LLR Deku Scrub Grotto Right", function() return can_stun_deku end)
+Adult_LLR_Grotto:connect_one_way("Adult LLR Deku Scrub Grotto Center", function() return can_stun_deku end)
+Adult_LLR_Grotto:connect_one_way("Adult LLR Grotto Beehive", function() return can_break_upper_beehive end )
+
+Child_LLR_Grotto:connect_one_way_entrance("Child Lon Lon Ranch", Child_Lon_Lon_Ranch)
+Adult_LLR_Grotto:connect_one_way_entrance("Adult Lon Lon Ranch", Adult_Lon_Lon_Ranch)
 {
     "region_name": "Lon Lon Ranch",
     "scene": "Lon Lon Ranch",
