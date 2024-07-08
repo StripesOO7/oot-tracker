acbf2be9e90a0329bdc48255af0903994594a106
StripesOO7 <54711792+StripeesOO7@users.noreply.github.com>
2024-07-08 17:34:02 +0200

    fixed 10 big poes  always being doable and adds base wallet

--- scripts/logic/Market.lua
+++ scripts/logic/Market.lua
@@ -229,7 +229,7 @@ Child_Market_Guard_House:connect_one_way("Child Market GS Guard House", function
 
 Adult_Market_Guard_House:connect_one_way("Adult Market 10 Big Poes", function() 
     return Any(
-        Big_Poe(), 
+        -- Big_Poe(), 
         Has("Bottle")--, 
         -- Tracker:FindObjectForCode("big_poe_count").AcquiredCount
     ) 
